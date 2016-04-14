b02e69ccdfd405c64d1cac9c41bec1bbdcc3f2e9
LucasMW <great-lucas@hotmail.com>
2016-04-14 00:24:06 -0300

    added four operations

--- calculator.lua
+++ calculator.lua
@@ -4,26 +4,46 @@
 -- version: 0.0.1
 -- size:  ?
 
+local add, sub, mult, div, no_op = 0, 1, 2, 4, 8
+function chooseOperation(str)
+  if string.match(str, "add") then
+    print ("enter next number")
+    number = tonumber(io.read())
+    acc = acc + number
+  elseif string.match(str, "sub")  then
+    print ("sub")
+    print ("enter next number")
+    number = tonumber(io.read())
+    acc = acc - number
+  elseif string.match(str, "mult")  then
+    print ("mult")
+    print ("enter next number")
+    number = tonumber(io.read())
+    acc = acc * number
+  elseif string.match(str, "div")  then
+    print ("div")
+    print ("enter next number")
+    number = tonumber(io.read())
+    acc = acc / number
+  else
+    print("unknown error")
+  end
+  print(string.format("acc: %f",acc))
+end
 print("Simple Calcultator")
 cmdStr = io.read()
+mode = no_op
 
 acc  = 0-- var to represent the current result
 
 
 while(cmdStr ~= "quit")  do
 print("Received " .. cmdStr)
-
+chooseOperation(cmdStr)
 cmdStr = io.read()
 end
 
-function chooseOperation(str)
-  if string.match(str, "add") then
-    print ("add")
- 
-  else
-    print("unknown error")
-  end
-end
+
 
 function tranformStr(str)
   print(str.byte);
