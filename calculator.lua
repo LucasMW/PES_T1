3a2b0da47a4f564381964273d35b77b20391db84
LucasMW <great-lucas@hotmail.com>
2016-04-13 23:40:07 -0300

    planned function to call for operation decision

--- calculator.lua
+++ calculator.lua
@@ -1,24 +1,28 @@
 -- titulo: Criptografador  
 -- autor: Lucas Menezes
 -- data: 12/04/2016
--- version: 0.0.0
+-- version: 0.0.1
 -- size:  ?
 
 print("Simple Calcultator")
 cmdStr = io.read()
 
+acc  = 0-- var to represent the current result
 
-acc -- var to represent the current result
 
-while(cmdStr ~= "quit") 
-do
+while(cmdStr ~= "quit")  do
 print("Received " .. cmdStr)
 
 cmdStr = io.read()
 end
 
-function chooseOperation()
-  
+function chooseOperation(str)
+  if string.match(str, "add") then
+    print ("add")
+ 
+  else
+    print("unknown error")
+  end
 end
 
 function tranformStr(str)
